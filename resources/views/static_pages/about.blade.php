3fbaffd7c4016dfdd1eee4b793464eecaa13755c
luhix <zhishiniao@126.com>
2019-05-18 16:05:57 +0800

    基础页面

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('title','关于')
+
+@section('content')
+    <h2>关于</h2>
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('title', '帮助')
+
+@section('content')
+    <h2>帮助</h2>
+@stop
