2d8703cb223e62ff5c72535db09dcd55b136c0a9
luhix <zhishiniao@126.com>
2019-05-21 14:35:23 +0800

    管理员可删除用户

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,5 +21,7 @@ $factory->define(App\Models\User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => Str::random(10),
+        'created_at' => $faker->dateTimeBetween('-3 year', '-1 year'),//  生成时间在 三年到一年 之间 否则不会生成时间
+        'updated_at' => $faker->dateTimeBetween('-1 year', '-5 month'),//  生成时间在 一年到五个月之间 不配置不会生成
     ];
 });
