5f13f71e93d42afb9b5e4f73577996ad3e8c06ef
luhix <zhishiniao@126.com>
2019-05-22 14:15:17 +0800

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -71,4 +71,55 @@ class User extends Authenticatable
         return $this->statuses()
             ->orderBy('created_at', 'desc');
     }
+
+    /**
+     *
+     * @DESC: 获取粉丝关系列表
+     *
+     * @author: HX
+     * @Time: 2019/5/22   14:03
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    /**
+     *
+     * @DESC: 获取粉丝用户关注人列表
+     *
+     * @author: HX
+     * @Time: 2019/5/22   14:04
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function followings()
+    {
+        return $this->belongsToMany(User::class,'followers','follower_id','user_id');
+    }
+    
+    public function follow($user_ids)
+    {
+        if ( ! is_array($user_ids) ) {
+            $user_ids = compact('user_ids');
+        }
+        
+        $this->followings()->sync($user_ids, false);
+    }
+    
+    public function unfollow($user_ids)
+    {
+        if ( !is_array($user_ids) ) {
+            $user_ids = compact('user_ids');
+        }
+        
+        $this->followings()->detach($user_ids);
+    }
+    
+    public function isFollowing($user_id)
+    {
+        return $this->followings()->contains($user_id);
+    }
 }
